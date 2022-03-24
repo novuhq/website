0fa01bfcc8c93c1fb4df4e631841d1922fa1f446
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-03-24 23:47:06 +0300

    feat: set styles for Link

--- src/components/shared/link/link.jsx
+++ src/components/shared/link/link.jsx
@@ -1,23 +1,64 @@
 import clsx from 'clsx';
+import { motion, useAnimation } from 'framer-motion';
 import { Link as GatsbyLink } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 
-// Example of the code — https://user-images.githubusercontent.com/20713191/144221096-1939c382-4ab8-4d28-b0e6-7bbe3a8f8556.png
+import DEFAULT_EASE from 'constants/default-ease';
+
 const styles = {
-  // TODO: Add base styles
-  base: '',
-  // TODO: Add sizes. Better to write down all sizes and go from higher to lower, e.g. "xl", "lg", "md", "sm", "xs"
-  //       The name of the size cannot be lower than the font size that being used, e.g. "sm" size cannot have font-size "xs"
-  //       Check out an example by a link above for better understanding
-  size: {},
-  // TODO: Add themes. Better to name the theme using this pattern: "${color-name}-${theme-type}", e.g. "black-filled"
-  //       If there is no dividing between theme types, then feel free to use just color names, e.g. "black"
-  //       Check out an example by a link above for better understanding
-  theme: {},
+  base: 'inline-block leading-none',
+  size: {
+    md: 'text-base',
+    sm: 'text-sm',
+  },
+  theme: {
+    'primary-underline':
+      'text-primary-2 relative tracking-wide uppercase pb-1.5 transition-colors duration-200 hover:text-primary-2',
+    white: 'text-white hover:text-primary-2 transition-colors',
+  },
+};
+
+const underlineVariants = {
+  initial: {
+    right: 0,
+    left: 'auto',
+    width: '100%',
+    scaleX: -1,
+  },
+  start: {
+    right: 0,
+    left: 'auto',
+    width: 0,
+    scaleX: -1,
+    transition: {
+      duration: 0.25,
+      ease: DEFAULT_EASE,
+    },
+    transitionEnd: {
+      right: 'auto',
+      left: 0,
+      scaleX: 1,
+    },
+  },
+  finish: {
+    width: '100%',
+    transition: {
+      duration: 0.25,
+      ease: DEFAULT_EASE,
+    },
+    transitionEnd: {
+      right: 0,
+      left: 'auto',
+      scaleX: -1,
+    },
+  },
 };
 
 const Link = ({ className: additionalClassName, size, theme, to, children, ...props }) => {
+  const [canAnimate, setCanAnimate] = useState(true);
+  const controls = useAnimation();
+
   const className = clsx(
     size && theme && styles.base,
     styles.size[size],
@@ -25,17 +66,49 @@ const Link = ({ className: additionalClassName, size, theme, to, children, ...pr
     additionalClassName
   );
 
+  const handleHover = () => {
+    if (!canAnimate) return;
+
+    setCanAnimate(false);
+
+    controls.start('start').then(() => controls.start('finish').then(() => setCanAnimate(true)));
+  };
+
+  const isUnderline = theme === 'primary-underline';
+
+  const underline = (
+    <motion.span
+      className="absolute bottom-0 left-0 h-px w-full rounded-full bg-primary-2"
+      initial="initial"
+      variants={underlineVariants}
+      animate={controls}
+      aria-hidden
+    />
+  );
+
   if (to.startsWith('/')) {
     return (
-      <GatsbyLink className={className} to={to} {...props}>
+      <GatsbyLink
+        className={className}
+        to={to}
+        onMouseEnter={isUnderline ? handleHover : undefined}
+        {...props}
+      >
         {children}
+        {isUnderline && underline}
       </GatsbyLink>
     );
   }
 
   return (
-    <a className={className} href={to} {...props}>
+    <a
+      className={className}
+      href={to}
+      onMouseEnter={isUnderline ? handleHover : undefined}
+      {...props}
+    >
       {children}
+      {isUnderline && underline}
     </a>
   );
 };
