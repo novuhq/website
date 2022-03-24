d1f3ae581adb96cbfc58d18ca2a66ad07f70473f
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-03-25 00:35:42 +0300

    feat: add Logos component

---
+++ src/components/pages/home/logos/logos.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import logo5 from './images/ab-in-bev.svg';
+import logo2 from './images/buzzfeed.svg';
+import logo1 from './images/google.svg';
+import logo4 from './images/relive.svg';
+import logo7 from './images/rvezy.svg';
+import logo6 from './images/tesla.svg';
+import logo3 from './images/typeform.svg';
+
+const TITLE = 'Loved by engineers from';
+const LOGOS = [logo1, logo2, logo3, logo4, logo5, logo6, logo7];
+
+const Logos = () => (
+  <section className="safe-paddings bg-black pt-20 pb-20">
+    <div className="container">
+      <h3 className="text-md text-center font-light text-white">{TITLE}</h3>
+      <ul className="mt-8 flex w-full justify-between">
+        {LOGOS.map((item, index) => (
+          <li className="block h-10 min-w-min" key={index}>
+            <img className="w-auto" src={item} loading="eager" alt={`logo-${index}`} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  </section>
+);
+
+export default Logos;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Hero from 'components/pages/home/hero';
 import HowItWorks from 'components/pages/home/how-it-works';
+import Logos from 'components/pages/home/logos';
 import Layout from 'components/shared/layout';
 
 const HomePage = () => (
   <Layout>
     <Hero />
+    <Logos />
     <HowItWorks />
   </Layout>
 );
