ab7e140b24a2beab515f4229efce234ae43effe3
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-03-24 23:26:25 +0300

    feat: set styles for Button

--- src/components/shared/button/button.jsx
+++ src/components/shared/button/button.jsx
@@ -6,16 +6,18 @@ import Link from 'components/shared/link';
 
 // Example of the code — https://user-images.githubusercontent.com/20713191/144215307-35538500-b9f0-486d-abed-1a14825bb75c.png
 const styles = {
-  // TODO: Add base styles
-  base: '',
-  // TODO: Add sizes. Better to write down all sizes and go from higher to lower, e.g. "xl", "lg", "md", "sm", "xs"
-  //       The name of the size cannot be lower than the font size that being used, e.g. "sm" size cannot have font-size "xs"
-  //       Check out an example by a link above for better understanding
-  size: {},
-  // TODO: Add themes. Better to name the theme using this pattern: "${color-name}-${theme-type}", e.g. "black-filled"
-  //       If there is no dividing between theme types, then feel free to use just color names, e.g. "black"
-  //       Check out an example by a link above for better understanding
-  theme: {},
+  base: 'inline-flex items-center justify-center !leading-none text-center whitespace-nowrap rounded transition-[colors, opacity] duration-200 outline-none uppercase font-medium',
+  size: {
+    sm: 'h-12 px-6 text-sm',
+    xs: 'h-10 px-5 text-xs',
+  },
+  theme: {
+    primary: 'bg-primary-2 text-black hover:bg-white',
+    'black-filled': 'bg-black text-white hover:bg-gray-4',
+    'white-filled': 'bg-white text-black hover:opacity-80',
+    'black-outline': 'bg-white text-black border border-black hover:bg-gray-5 hover:border-gray-5',
+    'gray-outline': 'bg-black text-white border border-gray-5 hover:bg-gray-5',
+  },
 };
 
 const Button = ({ className: additionalClassName, to, size, theme, children, ...otherProps }) => {
