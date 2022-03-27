c510c97f1cfc526a1f004c24be5e25a3fec999aa
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-03-27 15:21:28 +0300

    feat: add the Separator

---
+++ src/components/shared/separator/separator.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Separator = () => (
+  <div className="separator container relative after:absolute after:z-10 after:w-full after:border-t after:border-dashed after:border-gray-4" />
+);
+
+export default Separator;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,12 +5,14 @@ import Hero from 'components/pages/home/hero';
 import HowItWorks from 'components/pages/home/how-it-works';
 import Logos from 'components/pages/home/logos';
 import Layout from 'components/shared/layout';
+import Separator from 'components/shared/separator';
 
 const HomePage = () => (
   <Layout>
     <Hero />
     <Logos />
     <HowItWorks />
+    <Separator />
     <Features />
   </Layout>
 );
