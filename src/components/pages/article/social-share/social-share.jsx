e27283ac596d92dc3da074e0449581fb97282eeb
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-04-15 18:57:20 +0300

    feat: add the SocialShare component

---
+++ src/components/pages/article/social-share/social-share.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { LinkedinShareButton, TwitterShareButton, FacebookMessengerShareButton } from 'react-share';
+
+import AuthorWithDate from 'components/shared/author-with-date';
+
+import FacebookIcon from './images/facebook.inline.svg';
+import LinkedinIcon from './images/linkedin.inline.svg';
+import TwitterIcon from './images/twitter.inline.svg';
+
+const SocialShare = ({ author, date, url }) => (
+  <div className="relative flex items-center justify-between pt-8">
+    <AuthorWithDate author={author} date={date} />
+
+    <div className="flex space-x-4">
+      <TwitterShareButton url={url} resetButtonStyle={false}>
+        <TwitterIcon className="h-6 transition-opacity duration-200 hover:opacity-80" />
+      </TwitterShareButton>
+      <LinkedinShareButton url={url} resetButtonStyle={false}>
+        <LinkedinIcon className="h-6 transition-opacity duration-200 hover:opacity-80" />
+      </LinkedinShareButton>
+      <FacebookMessengerShareButton url={url} resetButtonStyle={false}>
+        <FacebookIcon className="h-6 transition-opacity duration-200 hover:opacity-80" />
+      </FacebookMessengerShareButton>
+    </div>
+  </div>
+);
+
+SocialShare.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.shape({
+      alternativeText: PropTypes.string,
+      localFile: PropTypes.shape({
+        childImageSharp: PropTypes.shape({
+          gatsbyImageData: PropTypes.any.isRequired,
+        }).isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+  date: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
+
+export default SocialShare;
