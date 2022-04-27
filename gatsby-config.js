014f7528154da32f3edb8aa14788004ab4f0df08
Dima Grossman <dima@grossman.io>
2022-04-27 14:30:21 +0300

    chore: update site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = {
   flags: { DEV_SSR: process.env.GATSBY_DEV_SSR || false },
   siteMetadata: {
-    siteTitle: 'Novu - Notification management simplified.',
+    siteTitle: 'Novu - The open-source notification infrastructure',
     siteDescription:
       'The ultimate library for managing multi-channel transactional notifications with a single API.',
     siteImage: '/images/social-preview.jpg',
