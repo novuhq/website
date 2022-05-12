14eef2a3f62e1ab5e6d775f27843d00f6af3d25c
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-05-12 16:45:32 +0300

    chore: add small adjustment

--- src/components/shared/banner/banner.jsx
+++ src/components/shared/banner/banner.jsx
@@ -8,11 +8,17 @@ import illustrationLeft from './images/illustration-left.svg';
 import illustrationRight from './images/illustration-right.svg';
 
 const Banner = () => (
-  <Link className="relative z-20 block overflow-hidden bg-gray-3 py-2.5" to="/blog/seed-funding">
+  <Link
+    className="group relative z-20 block overflow-hidden bg-gray-3 py-2.5"
+    to="/blog/seed-funding"
+  >
     <div className="container relative flex items-center justify-center">
       <p className="banner-text">
         We are thrilled to announce our <span>$6.6m</span> seed funding
-        <ArrowIcon className="ml-2.5 inline-block h-2.5 w-5" arria-hidden />
+        <ArrowIcon
+          className="ml-2.5 inline-block h-2.5 w-5 transition-colors duration-200 group-hover:text-primary-1"
+          arria-hidden
+        />
       </p>
 
       <img
