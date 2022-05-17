d397039b550ef61733aa653ed2fefddd5e0bedec
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-05-17 19:27:19 +0300

    feat: add seo metadata

--- src/pages/contributor.jsx
+++ src/pages/contributor.jsx
@@ -7,21 +7,31 @@ import GetStarted from 'components/pages/home/get-started/get-started';
 import Layout from 'components/shared/layout';
 import Separator from 'components/shared/separator';
 
-const ContributorPage = () => (
-  <Layout>
-    <div className="safe-paddings pt-44 md:pt-30 sm:pt-22">
-      <div className="container-lg grid grid-cols-12 items-start gap-x-8 lg:gap-x-7 md:flex md:flex-col md:gap-x-0">
-        <Profile />
-        <div className="col-span-8">
-          <Achievements />
-          <Separator className="py-20 px-0 sm:py-16" backgroundColor="black" />
-          <Activity />
+const ContributorPage = () => {
+  const seo = {
+    title: 'Novu - Contributor',
+    description:
+      'The ultimate library for managing multi-channel transactional notifications with a single API.',
+    slug: 'contributor',
+    // TODO: this value allows you not to index the page in search engines, once the page is ready remove this value
+    preventIndexing: true,
+  };
+  return (
+    <Layout seo={seo}>
+      <div className="safe-paddings pt-44 md:pt-30 sm:pt-22">
+        <div className="container-lg grid grid-cols-12 items-start gap-x-8 lg:gap-x-7 md:flex md:flex-col md:gap-x-0">
+          <Profile />
+          <div className="col-span-8">
+            <Achievements />
+            <Separator className="py-20 px-0 sm:py-16" backgroundColor="black" />
+            <Activity />
+          </div>
         </div>
       </div>
-    </div>
-    <GetStarted />
-    <Separator backgroundColor="black" />
-  </Layout>
-);
+      <GetStarted />
+      <Separator backgroundColor="black" />
+    </Layout>
+  );
+};
 
 export default ContributorPage;
