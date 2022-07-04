395e5d64bcce230044f60d96079706ba2c5ad78a
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-07-04 17:11:30 +0300

    feat: responsive version of the Hero section

--- src/components/pages/podcast/hero/hero.jsx
+++ src/components/pages/podcast/hero/hero.jsx
@@ -1,7 +1,6 @@
 import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 
-import Heading from 'components/shared/heading';
 import Link from 'components/shared/link';
 import ApplePodcastIcon from 'icons/apple-podcast.inline.svg';
 import SpotifyIcon from 'icons/spotify.inline.svg';
@@ -16,49 +15,48 @@ const DESCRIPTION = `A podcast by Novu where our hosts <strong>Nevo David</stron
 
 const Hero = () => (
   <section className="safe-paddings relative overflow-hidden pt-36 pb-28 lg:pt-32 md:pt-28 md:pb-10 sm:pt-18">
-    <div className="container relative z-10 grid grid-cols-12 gap-x-8 lg:gap-x-7 md:flex md:flex-col">
-      <div className="col-start-1 col-end-7 flex flex-col md:order-2 md:mt-5">
-        <Heading
-          className="text-highlighting-colorful-gradient mt-4 text-[72px] font-bold leading-none sm:text-3xl"
-          size="2xl"
-          tag="h1"
-          theme="white"
-          asHTML
-        >
-          {TITLE}
-        </Heading>
+    <div className="container relative z-10 grid grid-cols-12 gap-x-8 lg:gap-x-7 sm:flex sm:flex-col sm:gap-0 sm:space-y-10">
+      <div className="col-start-1 col-end-7 flex flex-col">
+        <h1
+          className="text-highlighting-colorful-gradient mt-4 text-[72px] font-bold leading-none lg:text-6xl md:text-4xl sm:text-[32px]"
+          dangerouslySetInnerHTML={{ __html: TITLE }}
+        />
         <p
-          className="mt-6 max-w-[590px] text-lg font-book leading-tight text-gray-9"
+          className="mt-6 max-w-[590px] text-lg font-book leading-tight text-gray-9 lg:mt-5 md:mt-4 md:text-base"
           dangerouslySetInnerHTML={{ __html: DESCRIPTION }}
         />
 
-        <div className="mt-7 flex space-x-6">
+        <div className="mt-7 flex space-x-6 lg:mt-6 md:mt-5 md:space-x-4">
           <Link
-            className="flex items-center space-x-2 rounded-md border border-gray-4 p-2 pr-4"
+            className="flex items-center space-x-2 rounded-md border border-gray-4 p-2 pr-4 sm:pr-2"
             to="/"
           >
-            <ApplePodcastIcon className="h-9" />
+            <ApplePodcastIcon className="h-9 flex-shrink-0 md:h-8" />
             <div className="flex flex-col">
-              <span className="text-xs leading-none">Listen on</span>
-              <span className="mt-1 font-bold leading-none">Apple Podcasts</span>
+              <span className="text-xs leading-none md:text-[10px]">Listen on</span>
+              <span className="mt-1 font-bold leading-none md:mt-0.5 xs:text-sm xs:font-medium">
+                Apple Podcasts
+              </span>
             </div>
           </Link>
           <Link
-            className="flex items-center space-x-2 rounded-md border border-gray-4 p-2 pr-4"
+            className="flex items-center space-x-2 rounded-md border border-gray-4 p-2 pr-4 sm:pr-2"
             to="/"
           >
-            <SpotifyIcon className="h-9" />
+            <SpotifyIcon className="h-9 flex-shrink-0 md:h-8" />
             <div className="flex flex-col">
-              <span className="text-xs leading-none">Listen on</span>
-              <span className="mt-1 font-bold leading-none">Spotify</span>
+              <span className="text-xs leading-none md:text-[10px]">Listen on</span>
+              <span className="mt-1 font-bold leading-none md:mt-0.5 xs:text-sm xs:font-medium">
+                Spotify
+              </span>
             </div>
           </Link>
         </div>
       </div>
 
-      <div className="col-start-7 col-end-13">
+      <div className="col-start-7 col-end-13 sm:mx-auto sm:max-w-[328px]">
         <StaticImage
-          className="-mt-[28px] -mb-[48px] -ml-[38px] max-w-[716px]"
+          className="-mt-[28px] -mb-[48px] -ml-[38px] max-w-[716px] lg:-my-5 lg:-ml-[26px] md:-my-3.5 md:-ml-5"
           src="./images/podcast-hero-illustration.png"
           alt=""
           loading="eager"
