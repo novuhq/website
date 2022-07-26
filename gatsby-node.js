53651ae7f75b238fff77291efb501179a2ab7dc2
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-07-26 19:51:52 +0300

    chore: create static pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,49 @@
-const fetch = require(`node-fetch`);
+const fs = require('fs');
 const path = require('path');
 
+const fetch = require(`node-fetch`);
 const slash = require('slash');
+
+async function createPages({ graphql, actions, reporter }) {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    {
+      allWpPage(filter: { template: { templateName: { ne: "Blog" } } }) {
+        nodes {
+          id
+          uri
+          template {
+            templateName
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    throw new Error(result.errors);
+  }
+
+  const pages = result.data.allWpPage.nodes;
+
+  pages.forEach(({ id, uri, template: { templateName } }) => {
+    const templateNamePath = templateName.toLowerCase().replace(/\s/g, '-');
+    const templatePath = path.resolve(`./src/templates/${templateNamePath}.jsx`);
+
+    if (fs.existsSync(templatePath)) {
+      createPage({
+        path: uri,
+        component: slash(templatePath),
+        context: {
+          id,
+        },
+      });
+    } else {
+      reporter.error(`Template "${templateName}" was not found`);
+    }
+  });
+}
 
 async function createBlogPages({ graphql, actions }) {
   const POSTS_PER_PAGE = 12;
@@ -276,6 +318,7 @@ exports.createPages = async (args) => {
     ...args,
   };
 
+  await createPages(params);
   await createBlogPages(params);
   await createPosts(params);
   await createPodcastPage(params);
