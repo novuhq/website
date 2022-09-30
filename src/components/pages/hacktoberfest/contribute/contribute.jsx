4d372503ff08059ac4722ea30fa0596693b909d7
Thi Van Le <vannnyle@gmail.com>
2022-09-30 13:28:02 +0200

    fix: replace inline svgs

--- src/components/pages/hacktoberfest/contribute/contribute.jsx
+++ src/components/pages/hacktoberfest/contribute/contribute.jsx
@@ -3,36 +3,36 @@ import React from 'react';
 import Heading from 'components/shared/heading';
 import Link from 'components/shared/link';
 
-import BlogIcon from './images/blog.inline.svg';
-import BuildIcon from './images/build.inline.svg';
-import ContributionsIcon from './images/contributions.inline.svg';
-import TutorialIcon from './images/tutorial.inline.svg';
+import blogIcon from './images/blog.svg';
+import buildIcon from './images/build.svg';
+import contributionsIcon from './images/contributions.svg';
+import tutorialIcon from './images/tutorial.svg';
 
 const TITLE = 'How to contribute';
 const ITEMS = [
   {
-    icon: ContributionsIcon,
+    icon: contributionsIcon,
     title: 'Contributions to open issues',
     description:
       'We have a curated list of Hacktoberfest issues that are ready for you to pick up.',
     url: '/',
   },
   {
-    icon: BuildIcon,
+    icon: buildIcon,
     title: 'Build a demo app',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sapien turpis elementum non.',
     url: '/',
   },
   {
-    icon: BlogIcon,
+    icon: blogIcon,
     title: 'Write a blog post',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sapien turpis elementum non.',
     url: '/',
   },
   {
-    icon: TutorialIcon,
+    icon: tutorialIcon,
     title: 'Make a tutorial',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sapien turpis elementum non.',
@@ -52,12 +52,20 @@ const Contribute = () => (
         {TITLE}
       </Heading>
       <ul className="mt-20 grid grid-cols-4 gap-x-10 lg:auto-rows-fr lg:grid-cols-2 lg:gap-y-16 lg:gap-x-16 md:mt-16 md:gap-x-10 md:gap-y-10 sm:mt-12 sm:grid-cols-1">
-        {ITEMS.map(({ icon: Icon, title, description, url }, index) => (
+        {ITEMS.map(({ icon, title, description, url }, index) => (
           <li
             className="flex flex-col items-start lg:max-w-sm sm:mx-auto sm:max-w-[410px] sm:items-center sm:text-center"
             key={index}
           >
-            <Icon className="h-auto w-30 md:w-28" />
+            <img
+              className="h-auto w-30 md:w-28"
+              src={icon}
+              alt=""
+              width={120}
+              height={64}
+              loading="lazy"
+              aria-hidden
+            />
             <Heading className="mt-5 font-medium sm:text-xl" tag="h3" theme="white" size="xs">
               {title}
             </Heading>
