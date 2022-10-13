ed078b0a82b19def20d92da7acc1f1c2afb1657d
Nevo David <nevo@notu.co>
2022-10-13 17:17:19 +0700

    feat: events

--- src/components/pages/hacktoberfest/events/events.jsx
+++ src/components/pages/hacktoberfest/events/events.jsx
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import React from 'react';
 
 import Button from 'components/shared/button';
@@ -9,124 +10,120 @@ const DESCRIPTION =
 const ITEMS = [
   {
     title: 'Hacktoberfest Kick-Off',
-    date: 'October 3',
-    time: '08:00 PM EST',
-    description: 'Eastern daylight time',
+    date: moment.utc('2022-10-04T00:00:00'),
+    description: '',
     link: 'https://www.twitch.tv/iampearceman',
   },
   {
     title: 'Twitter Spaces',
-    date: 'October 6',
-    time: '11:00 AM EST',
-    description: 'An -audio-only discussion session about open-source technologies, carear-path, best use cases, and much more.',
+    date: moment.utc('2022-10-06T15:00:00'),
+    description:
+      'An -audio-only discussion session about open-source technologies, carear-path, best use cases, and much more.',
     link: 'https://twitter.com/i/spaces/1rmxPkWaAeLJN',
   },
   {
-    title: 'Meet The Contributor',
-    date: 'October 6',
-    time: '12:00 PM EST',
-    description: 'An ask-me-anything-like format discussion, with open source maintainers and contributors, where the host and the audience explore their motivations, world view, career path, technologies preferences, and much more!',
+    title: 'Meet Contributors',
+    date: moment.utc('2022-10-06T16:00:00'),
+    description:
+      'An ask-me-anything-like format discussion, with open source maintainers and contributors, where the host and the audience explore their motivations, world view, career path, technologies preferences, and much more!',
     link: 'https://www.twitch.tv/iampearceman',
   },
   {
-    title: 'Open Source GeekOut Stream',
-    date: 'October 7',
-    time: '7:00 AM EST',
-    description: 'Live stream where we discuss current tech, approaches, ideas, and projects and have a good time with the audience.',
+    title: 'GeekOut Stream',
+    date: moment.utc('2022-10-07T11:00:00'),
+    description:
+      'Live stream where we discuss current tech, approaches, ideas, and projects and have a good time with the audience.',
     link: 'https://www.twitch.tv/iampearceman',
   },
   {
-    title: 'Open Source GeekOut Stream',
-    date: 'October 14',
-    time: '7:00 AM EST',
-    description: 'Live stream where we discuss current tech, approaches, ideas, and projects and have a good time with the audience.',
-    link: 'https://www.twitch.tv/iampearceman',
+    title: 'GeekOut Stream',
+    date: moment.utc('2022-10-14T11:00:00'),
+    description:
+      'Live stream where we discuss current tech, approaches, ideas, and projects and have a good time with the audience.',
+    link: 'https://zoom.us/webinar/register/WN_j-ZtggW2TimqWx5FbspHoA',
   },
   {
-    title: 'Meet The Contributor',
-    date: 'October 17',
-    time: '8:00 AM EST',
-    description: 'An ask-me-anything-like format discussion, with open source maintainers and contributors, where the host and the audience explore their motivations, world view, career path, technologies preferences, and much more!',
-    link: 'https://www.twitch.tv/iampearceman',
+    title: 'Meet Contributors',
+    date: moment.utc('2022-10-17T12:00:00'),
+    description:
+      'An ask-me-anything-like format discussion, with open source maintainers and contributors, where the host and the audience explore their motivations, world view, career path, technologies preferences, and much more!',
+    link: 'https://zoom.us/webinar/register/WN_0ae0nStASqC9N8brrn21fA',
   },
   {
-    title: 'Novu Special with Vincent Ge (Appwrite)',
-    date: 'October 17',
-    time: '12:00 PM EST',
-    description: 'Live event, usually in an interview / workshop or a webinar form, where we host special guests from the tech industry with specific expertise to share their knowledge, skills, and insights.',
-    link: 'https://www.twitch.tv/iampearceman',
+    title: 'Novu with Appwrite',
+    date: moment.utc('2022-10-17T16:00:00'),
+    description:
+      'Live event, usually in an interview / workshop or a webinar form, where we host special guests from the tech industry with specific expertise to share their knowledge, skills, and insights.',
+    link: 'https://zoom.us/webinar/register/WN_d0sMzXKwS0OTzY7BMU_sLQ',
   },
   {
     title: 'Novu AMA Session',
-    date: 'October 18',
-    time: '11:00 AM EST',
-    description: 'Eastern daylight time',
-    link: 'https://www.twitch.tv/iampearceman',
+    date: moment.utc('2022-10-18T15:00:00'),
+    description: '',
+    link: 'https://zoom.us/webinar/register/WN_C6laJ2hqQeGI0JbGHKD_YA',
   },
   {
     title: 'Novu Live Demo',
-    date: 'October 19',
-    time: '11:00 AM EST',
-    description: 'Eastern daylight time',
-    link: 'https://www.twitch.tv/iampearceman',
+    date: moment.utc('2022-10-19T15:00:00'),
+    description: '',
+    link: 'https://zoom.us/webinar/register/WN_7JTEufxDTmG0B3m3ilvuMQ',
   },
   {
-    title: 'Novu Special with Hila Fish (Wix)',
-    date: 'October 20',
-    time: '01:00 PM EST',
-    description: 'Live event, usually in an interview / workshop or a webinar form, where we host special guests from the tech industry with specific expertise to share their knowledge, skills, and insights.',
-    link: 'https://www.twitch.tv/iampearceman',
+    title: 'Novu with Wix',
+    date: moment.utc('2022-10-20T17:00:00'),
+    description:
+      'Live event, usually in an interview / workshop or a webinar form, where we host special guests from the tech industry with specific expertise to share their knowledge, skills, and insights.',
+    link: 'https://zoom.us/webinar/register/WN_clfNRgknSwKINke1vobGyA',
   },
   {
-    title: 'Open Source GeekOut Stream',
-    date: 'October 21',
-    time: '7:00 AM EST',
-    description: 'Live stream where we discuss current tech, approaches, ideas, and projects and have a good time with the audience.',
-    link: 'https://www.twitch.tv/iampearceman',
+    title: 'GeekOut Stream',
+    date: moment.utc('2022-10-21T11:00:00'),
+    description:
+      'Live stream where we discuss current tech, approaches, ideas, and projects and have a good time with the audience.',
+    link: 'https://zoom.us/webinar/register/WN_5FH8pt8bS562fipvnuhWFA',
   },
   {
     title: 'Pull Request Party',
-    date: 'October 25',
-    time: '07:00 AM EST',
-    description: 'Live event where Novu’s core team, moderators, and contributors are helping people to create their first Pull Request, solve problems and conflicts, do live code reviews, and much more!',
-    link: 'https://www.twitch.tv/iampearceman',
+    date: moment.utc('2022-10-25T11:00:00'),
+    description:
+      'Live event where Novu’s core team, moderators, and contributors are helping people to create their first Pull Request, solve problems and conflicts, do live code reviews, and much more!',
+    link: 'https://zoom.us/webinar/register/WN_0dQOotoeTZOU29IyaGvnHg',
   },
   {
     title: 'Office Hours',
-    date: 'October 25',
-    time: '11:00 AM EST',
-    description: 'Community-based live event where Novu’s core team members show and tell about the latest features, development process, roadmap view, discuss potential plans, feature ideas,  and answer questions.',
+    date: moment.utc('2022-10-25T15:00:00'),
+    description:
+      'Community-based live event where Novu’s core team members show and tell about the latest features, development process, roadmap view, discuss potential plans, feature ideas,  and answer questions.',
     link: 'https://discord.com/events/895029566685462578/1025554506504732702',
   },
   {
-    title: 'Collaboration Event by AppWrite',
-    date: 'October 27',
-    time: '12:00 PM EST',
-    description: 'AppWrite is hosting Novu’s team members to showcase the project, demonstrate how it works and how to contribute.',
+    title: 'Novu with AppWrite',
+    date: moment.utc('2022-10-27T16:00:00'),
+    description:
+      'AppWrite is hosting Novu’s team members to showcase the project, demonstrate how it works and how to contribute.',
     link: 'https://www.youtube.com/watch?v=obRy9Afsxs0&ab_channel=Appwrite',
   },
   {
-    title: 'Open Source GeekOut Stream',
-    date: 'October 28',
-    time: '7:00 AM EST',
-    description: 'Live stream where we discuss current tech, approaches, ideas, and projects and have a good time with the audience.',
-    link: 'https://www.twitch.tv/iampearceman',
+    title: 'GeekOut Stream',
+    date: moment.utc('2022-10-28T11:00:00'),
+    description:
+      'Live stream where we discuss current tech, approaches, ideas, and projects and have a good time with the audience.',
+    link: 'https://zoom.us/webinar/register/WN_DolrAwgLSKOGXEFR7vk36Q',
   },
   {
-    title: 'Novu Special with Joshua Poddoku',
-    date: 'October 30',
-    time: '10:00 AM EST',
-    description: 'Live event, usually in an interview / workshop or a webinar form, where we host special guests from the tech industry with specific expertise to share their knowledge, skills, and insights.',
-    link: 'https://www.twitch.tv/iampearceman',
+    title: 'Novu with Joshua',
+    date: moment.utc('2022-10-30T14:00:00'),
+    description:
+      'Live event, usually in an interview / workshop or a webinar form, where we host special guests from the tech industry with specific expertise to share their knowledge, skills, and insights.',
+    link: 'https://zoom.us/webinar/register/WN_hx7WZgd8QdOFvDmQTC13lg',
   },
   {
-    title: 'Hacktoberfest Closing Event',
-    date: 'October 31',
-    time: '10:00 AM EST',
-    description: 'Live event where we celebrate the end of this amazing month, going over significant contributions, sharing insights and upcoming events, along with thanking all our collaborators and partners.',
-    link: 'https://www.twitch.tv/iampearceman',
+    title: 'Closing Event',
+    date: moment.utc('2022-10-31T14:00:00'),
+    description:
+      'Live event where we celebrate the end of this amazing month, going over significant contributions, sharing insights and upcoming events, along with thanking all our collaborators and partners.',
+    link: 'https://zoom.us/webinar/register/WN_FvBZpu7xQM29hOgbQGCUmg',
   },
-  
 ];
 const Events = () => (
   <section className="events safe-paddings mt-8 py-32 md:py-20 sm:py-16">
@@ -138,33 +135,33 @@ const Events = () => (
         {DESCRIPTION}
       </p>
       <div className="mt-16 grid auto-rows-fr grid-cols-4 gap-x-10 gap-y-7 xl:grid-cols-2 md:mt-12 sm:mt-10 sm:auto-rows-auto sm:grid-cols-1 sm:gap-y-6">
-        {ITEMS.map(({ title, date, time, description, link }, index) => {
+        {ITEMS.map(({ title, date, description, link }, index) => {
           const isExternal = link.startsWith('http');
           return (
             <div
               className="flex flex-col rounded-[20px] py-6 px-5"
               style={{
+                opacity: moment().isAfter(date) ? 0.5 : 1,
+                filter: moment().isAfter(date) ? 'grayscale(100%)' : 'grayscale(0%)',
+                pointerEvents: moment().isAfter(date) ? 'none' : '',
                 background:
                   'linear-gradient(180deg, #1A1A1A 0%, rgba(26, 26, 26, 0.7) 100%), #000000',
               }}
               key={index}
             >
               <Heading
-                className="mb-5 font-medium leading-snug md:text-xl"
+                className="mb-5 whitespace-nowrap font-medium leading-snug md:text-xl"
                 tag="h3"
                 size="xs"
                 theme="purple"
               >
                 {title}
               </Heading>
-              <div className="mt-auto flex shrink flex-col">
+              <div className="flex flex-1 shrink flex-col">
                 <span className="flex justify-center space-x-5 text-lg font-medium sm:text-base">
-                  <date>{date}</date>
-                  <time>{time}</time>
+                  <date>{date.local().format('LLL')}</date>
                 </span>
-                <span className="mt-1 mb-5 text-lg text-gray-9 sm:text-base">
-                  {description || 'TBD'}
-                </span>
+                <span className="mt-1 mb-5 text-lg text-gray-9 sm:text-base">{description}</span>
                 <Button
                   className="mt-auto h-7 self-center"
                   theme="yellow"
@@ -173,7 +170,7 @@ const Events = () => (
                   rel={isExternal ? 'noopener noreferrer' : null}
                   size="xs"
                 >
-                  Learn more
+                  RSVP NOW!
                 </Button>
               </div>
             </div>
