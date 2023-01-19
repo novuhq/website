c6b48082fd9fdc186f30abcc7e3123dc884ec152
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-01-19 22:01:58 +0300

    feat: add data for FAQ section

--- src/components/pages/pricing/faq/faq.jsx
+++ src/components/pages/pricing/faq/faq.jsx
@@ -1,24 +1,118 @@
 import React from 'react';
 
 import Heading from 'components/shared/heading';
+import Link from 'components/shared/link';
 
 import Question from './question';
 
 const FAQ_DATA = [
   {
-    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+    question: '🚧 What is a trigger event?',
     answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing massa sed ultrices sed felis volutpat ac. Congue sit nibh sed ipsum, erat facilisis mauris. Amet, est urna facilisi tempus ut amet. Pharetra orci curabitur faucibus purus in nibh. Dolor, sodales malesuada nec vitae scelerisque leo convallis ac dictumst. Euismod.',
+      'A trigger event (also called an event) is a request that kicks off a process in our trigger engine (API call to /v1/events/trigger for example). A trigger event can make many different types of actions, including digests, waiting, and/or sending notifications to various channels, as well filters and user preference checks. You are charged by us for the trigger event that starts a process in the trigger engine for each unique subscriber.',
   },
   {
-    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+    question: 'What is a notification?',
     answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing massa sed ultrices sed felis volutpat ac. Congue sit nibh sed ipsum, erat facilisis mauris. Amet, est urna facilisi tempus ut amet. Pharetra orci curabitur faucibus purus in nibh. Dolor, sodales malesuada nec vitae scelerisque leo convallis ac dictumst. Euismod.',
+      'A notification is any message sent over any channel, after Novu logic engine, Novu user preferences, and your configuration calculated and crafted the message. A notification can be sent to an email, In-App notification center, chat, push, and more.',
   },
   {
-    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+    question: 'What are the differences between Novu Cloud and Open Source?',
+    answer: (
+      <>
+        Novu{' '}
+        <Link to="https://github.com/novuhq/novu" target="_blank" rel="noreferrer" theme="primary">
+          Open source
+        </Link>{' '}
+        is a technology available under an MIT license, build and maintained by Novu community, you
+        can adapt it to your needs, contribute, or simply use docker files and run it. Novu Cloud is
+        a managed service run, managed, and maintained by Novu, Cloud version is scalable and robust
+        by design, and uptime SLAs, and includes more business-facing features, that are not
+        available under the Open Source version.
+      </>
+    ),
+  },
+  {
+    question: 'Can I send more than 1 million events a month on Novu Cloud?',
     answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing massa sed ultrices sed felis volutpat ac. Congue sit nibh sed ipsum, erat facilisis mauris. Amet, est urna facilisi tempus ut amet. Pharetra orci curabitur faucibus purus in nibh. Dolor, sodales malesuada nec vitae scelerisque leo convallis ac dictumst. Euismod.',
+      'Yes, you absolutely can. Our managed cloud system was built to scale with your usage, so you don’t have to worry about it.',
+  },
+  {
+    question: 'Do you offer any enterprise plan?',
+    answer: (
+      <>
+        Yes, we do. Enterprise plans are available for teams who require extended enterprise-grade
+        features, and specific SLAs or have unique concerns about PII, security, insurance, or
+        legal. Feel free to reach out to us over Intercom, or send us an email at{' '}
+        <Link to="mailto:sales@novu.co" theme="primary">
+          sales@novu.co
+        </Link>
+        .
+      </>
+    ),
+  },
+  {
+    question: 'Do you have offer enterprise support plan?',
+    answer:
+      'As part of our enterprise support, you’ll get our usual support channels as well as a Microsoft Teams/Slack/Discord channel. In case you want to add an enterprise support plan, with dedicated technical support, we are happy to offer that next to an active enterprise plan.',
+  },
+  {
+    question: 'How long is the open beta for? When do you plan to start charging?',
+    answer:
+      'We plan to convert to the paid version of Novu sometime between Q2 and Q4 of 2023. We will ensure that we contact all of our customers at least x months before the switch so that you have plenty of time to ensure your systems are not impacted.',
+  },
+  {
+    question: 'I need more events or enterprise features, can you help?',
+    answer: (
+      <>
+        We sure can. We are happy to work with enterprises and build Novu to support both their
+        needs as well as the grand community ones. From new features, security, and privacy, or if
+        you need to send dozens or hundreds of events a month, we are here to help. Feel free to
+        reach out to us over Chat, or send us an email at{' '}
+        <Link to="mailto:sales@novu.co" theme="primary">
+          sales@novu.co
+        </Link>
+        .
+      </>
+    ),
+  },
+  {
+    question: 'What should I do if I have regulatory or security issues with PII?',
+    answer: (
+      <>
+        We regularly work with big companies and are happy to help and support you with guidance,
+        and various compliances including reports to ease your security and legal team. If you have
+        very complicated PII needs, you can use our OS version, Novu Hybrid-Cloud enterprise plan,
+        or reach out to us at{' '}
+        <Link to="mailto:sales@novu.co" theme="primary">
+          sales@novu.co
+        </Link>
+        , or our support, or{' '}
+        <Link to="https://discord.gg/9wcGSf22PM" theme="primary">
+          Discord
+        </Link>
+        .
+      </>
+    ),
+  },
+  {
+    question: 'Do you promise any Service Level Agreement (SLA)?',
+    answer: (
+      <>
+        We sure do! First of all we promise uptime SLA for our cloud system for 99.99%. We also
+        offer an initial 2 days support SLA, but most enquiries on our Intercom or{' '}
+        <Link to="https://discord.gg/9wcGSf22PM" theme="primary">
+          Discord
+        </Link>{' '}
+        get a response in a couple of hours. For our enterprise plan we can offer upgraded SLA as
+        part of the commercial contract.
+      </>
+    ),
+  },
+  {
+    question: 'Can I use Novu free of charge?',
+    answer:
+      'Yes, you can. If you send less than 10K events per month, then Novu Cloud is entirely free. Another option is to deploy the Open-Source version of Novu onto your own infrastructure, but that does not give you the unique SLA and global redundancy we have in the Cloud version of Novu.',
   },
 ];
 
