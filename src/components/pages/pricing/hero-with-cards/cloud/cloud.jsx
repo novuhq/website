0155204cdc81e709b2820b3ae62103b63cbb92c6
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-01-20 21:28:06 +0300

    chore: remove the custom card

--- src/components/pages/pricing/hero-with-cards/cloud/cloud.jsx
+++ src/components/pages/pricing/hero-with-cards/cloud/cloud.jsx
@@ -31,7 +31,6 @@ const RANGES = {
   170: '3000000',
   180: '4500000',
   190: '5000000',
-  200: '5000000',
 };
 
 const tooltip =
@@ -167,7 +166,7 @@ const getPricingData = (rangeValue) => [
 ];
 
 const Cloud = ({ activeTier, setActiveTier, findActiveTier, rangeValue, setRangeValue }) => {
-  const maxValue = 200;
+  const maxValue = 190;
 
   const eventsFormatter = Intl.NumberFormat('en-US');
 
@@ -229,149 +228,109 @@ const Cloud = ({ activeTier, setActiveTier, findActiveTier, rangeValue, setRange
           </span>
         </div>
       </div>
+      <ul className="mt-12 grid auto-rows-max grid-cols-4 items-stretch justify-between gap-10 text-center xl:gap-6 lg:mx-24 lg:grid-cols-2 lg:gap-6 md:mx-0 md:mt-10 md:gap-7 sm:grid-cols-1">
+        {getPricingData(rangeValue).map(
+          (
+            { titles, name, description, prices, extraOvercharge, items, buttons, isOpenBeta },
+            index
+          ) => {
+            const isActive = activeTier === name;
 
-      {Number(rangeValue) < maxValue ? (
-        <ul className="mt-12 grid auto-rows-max grid-cols-4 items-stretch justify-between gap-10 text-center xl:gap-6 lg:mx-24 lg:grid-cols-2 lg:gap-6 md:mx-0 md:mt-10 md:gap-7 sm:grid-cols-1">
-          {getPricingData(rangeValue).map(
-            (
-              { titles, name, description, prices, extraOvercharge, items, buttons, isOpenBeta },
-              index
-            ) => {
-              const isActive = activeTier === name;
-
-              return (
-                <li
+            return (
+              <li
+                className={clsx(
+                  'relative mx-auto w-full max-w-[338px] overflow-hidden rounded-xl p-px text-center after:absolute after:inset-0 after:-z-10 after:rounded-xl after:bg-pink-yellow-gradient after:opacity-0 after:transition-all after:duration-500 after:ease-in-out xl:mx-0 xl:min-w-0 xl:max-w-none sm:mx-auto sm:max-w-[380px]',
+                  isActive && 'after:opacity-100'
+                )}
+                key={index}
+              >
+                {isOpenBeta && (
+                  <div className="absolute -top-2 -left-2 aspect-square w-24 overflow-hidden rounded-sm">
+                    <div className="absolute bottom-0 left-0 block w-square-diagonal origin-bottom-left -rotate-45 bg-primary-1 py-[1px] text-center text-xs font-medium text-black">
+                      Open Beta*
+                    </div>
+                  </div>
+                )}
+                <div
                   className={clsx(
-                    'relative mx-auto w-full max-w-[338px] overflow-hidden rounded-xl p-px text-center after:absolute after:inset-0 after:-z-10 after:rounded-xl after:bg-pink-yellow-gradient after:opacity-0 after:transition-all after:duration-500 after:ease-in-out xl:mx-0 xl:min-w-0 xl:max-w-none sm:mx-auto sm:max-w-[380px]',
-                    isActive && 'after:opacity-100'
+                    'mx-auto flex h-full min-w-[336px] max-w-[338px] flex-col items-center justify-between rounded-xl bg-gray-gradient p-8 text-center transition-all duration-500 ease-in-out xl:mx-0 xl:min-w-0 xl:max-w-none xl:px-4 lg:px-6 md:px-6',
+                    isActive && 'bg-active-gray-gradient'
                   )}
-                  key={index}
                 >
-                  {isOpenBeta && (
-                    <div className="absolute -top-2 -left-2 aspect-square w-24 overflow-hidden rounded-sm">
-                      <div className="absolute bottom-0 left-0 block w-square-diagonal origin-bottom-left -rotate-45 bg-primary-1 py-[1px] text-center text-xs font-medium text-black">
-                        Open Beta*
-                      </div>
-                    </div>
-                  )}
-                  <div
-                    className={clsx(
-                      'mx-auto flex h-full min-w-[336px] max-w-[338px] flex-col items-center justify-between rounded-xl bg-gray-gradient p-8 text-center transition-all duration-500 ease-in-out xl:mx-0 xl:min-w-0 xl:max-w-none xl:px-4 lg:px-3 md:px-6',
-                      isActive && 'bg-active-gray-gradient'
-                    )}
-                  >
-                    <div className="flex-flex-col space-y-4">
-                      <span className="text-lg font-medium uppercase leading-none">
-                        {titles[getNearestKey(titles)] || titles.default}
-                      </span>
-                      <p className="mx-auto min-h-[38px] max-w-[95%] text-sm leading-snug text-gray-8 xl:min-h-[71px] sm:min-h-0">
-                        {description}
+                  <div className="flex-flex-col space-y-4">
+                    <span className="text-lg font-medium uppercase leading-none">
+                      {titles[getNearestKey(titles)] || titles.default}
+                    </span>
+                    <p className="mx-auto min-h-[38px] max-w-[95%] text-sm leading-snug text-gray-8 xl:min-h-[71px] lg:min-h-[58px] sm:min-h-0">
+                      {description}
+                    </p>
+                  </div>
+
+                  <div className="mt-10 flex min-h-[81px] flex-col md:mt-5">
+                    {typeof prices[getNearestKey(prices)] === 'string' ? (
+                      <p className="text-6xl font-medium leading-none xl:text-5xl lg:text-6xl md:mt-5 sm:text-5xl">
+                        {prices[getNearestKey(prices)]}
                       </p>
-                    </div>
-
-                    <div className="mt-10 flex min-h-[81px] flex-col md:mt-5">
-                      {typeof prices[getNearestKey(prices)] === 'string' ? (
-                        <p className="text-6xl font-medium leading-none xl:text-5xl lg:text-6xl md:mt-5 sm:text-5xl">
-                          {prices[getNearestKey(prices)]}
+                    ) : (
+                      <>
+                        <p className="text-[72px] font-medium leading-none xl:text-8xl md:text-[72px] sm:text-8xl">
+                          <span className="relative">
+                            <span className="absolute -left-6 top-6 text-3xl">$</span>
+                            {prices[getNearestKey(prices)] || prices.default}
+                          </span>
                         </p>
-                      ) : (
-                        <>
-                          <p className="text-[72px] font-medium leading-none xl:text-8xl md:text-[72px] sm:text-8xl">
-                            <span className="relative">
-                              <span className="absolute -left-6 top-6 text-3xl">$</span>
-                              {prices[getNearestKey(prices)] || prices.default}
-                            </span>
-                          </p>
-                          <span className="mt-2 text-base leading-tight text-gray-8">
-                            per month
-                          </span>
-                        </>
-                      )}
-                    </div>
+                        <span className="mt-2 text-base leading-tight text-gray-8">per month</span>
+                      </>
+                    )}
+                  </div>
 
-                    <ul className="mb-14 mt-8 flex flex-col space-y-2 leading-tight xl:mt-5 md:mt-8">
-                      {items.map((item, index) => (
-                        <li className="flex items-center space-x-3 xl:space-x-1.5" key={index}>
-                          <CheckIcon className="h-1.5 w-2.5 shrink-0 text-primary-1" />
-                          <span className="text-left text-base lg:text-sm md:text-base">
-                            {item}
-                          </span>
-                        </li>
-                      ))}
-                    </ul>
-                    <div className="relative w-full">
-                      {buttons[getNearestKey(buttons)] ? (
+                  <ul className="mb-14 mt-8 flex flex-col space-y-2 leading-tight xl:mt-5 md:mt-8">
+                    {items.map((item, index) => (
+                      <li className="flex items-center space-x-3 xl:space-x-1.5" key={index}>
+                        <CheckIcon className="h-1.5 w-2.5 shrink-0 text-primary-1" />
+                        <span className="text-left text-base lg:text-sm md:text-base">{item}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <div className="relative w-full">
+                    {buttons[getNearestKey(buttons)] ? (
+                      <Button
+                        className="mt-auto w-full lg:text-xs"
+                        to={buttons[getNearestKey(buttons)].url}
+                        theme={isActive ? 'pink-to-yellow-gradient' : 'gray-outline'}
+                        size="sm"
+                      >
+                        {buttons[getNearestKey(buttons)].text}
+                      </Button>
+                    ) : (
+                      <>
                         <Button
                           className="mt-auto w-full lg:text-xs"
-                          to={buttons[getNearestKey(buttons)].url}
+                          to={buttons.default.url}
                           theme={isActive ? 'pink-to-yellow-gradient' : 'gray-outline'}
                           size="sm"
                         >
-                          {buttons[getNearestKey(buttons)].text}
+                          {buttons.default.text}
                         </Button>
-                      ) : (
-                        <>
-                          <Button
-                            className="mt-auto w-full lg:text-xs"
-                            to={buttons.default.url}
-                            theme={isActive ? 'pink-to-yellow-gradient' : 'gray-outline'}
-                            size="sm"
-                          >
-                            {buttons.default.text}
-                          </Button>
 
-                          {extraOvercharge && extraOvercharge[getNearestKey(extraOvercharge)] && (
-                            <span className="absolute -top-6 left-0 w-full text-xs font-book leading-tight text-gray-8 xl:text-[11px] lg:text-xs">
-                              * ${extraOvercharge[getNearestKey(extraOvercharge)]} for another 1K
-                              events extra/overcharge
-                            </span>
-                          )}
-                        </>
-                      )}
-                    </div>
+                        {extraOvercharge && extraOvercharge[getNearestKey(extraOvercharge)] && (
+                          <span className="absolute -top-6 left-0 w-full text-xs font-book leading-tight text-gray-8 xl:text-[11px] lg:text-xs">
+                            * ${extraOvercharge[getNearestKey(extraOvercharge)]} for another 1K
+                            events extra/overcharge
+                          </span>
+                        )}
+                      </>
+                    )}
                   </div>
-                </li>
-              );
-            }
-          )}
-          <p className="col-span-full mt-2 text-center text-sm leading-snug text-gray-8">
-            *During Open Beta, all tariffs except Enterprise are free to use.
-          </p>
-        </ul>
-      ) : (
-        <div className="mx-auto mt-12 mb-16 flex min-h-[458px] max-w-[338px] flex-col items-center justify-between rounded-xl bg-gray-gradient p-8 text-center xl:px-4 lg:min-h-[412px] lg:px-3 md:mt-10 md:min-h-0 md:px-6 xs:max-w-none">
-          <div className="flex-flex-col space-y-5">
-            <span className="text-lg font-medium uppercase leading-none">Custom</span>
-            <p className="min-h-[57px] text-sm leading-snug text-gray-8 xl:min-h-0">
-              Lorem ipsum dolor sit amet consectetur. Odio mi ac dui tristique ipsum. A netus est
-              tempus purus ut at nisl id sit mattis.
-            </p>
-          </div>
-          <span className="mt-10 mb-8 text-6xl font-medium leading-none xl:text-5xl lg:text-4xl md:mt-5 md:text-6xl sm:text-5xl">
-            Contact us
-          </span>
-
-          <ul className="mb-14 mt-8 flex w-full flex-col space-y-2 leading-tight">
-            <li className="flex items-center space-x-3">
-              <CheckIcon className="h-1.5 w-2.5 shrink-0 text-primary-1" />
-              <span>Volume discounts</span>
-            </li>
-            <li className="flex items-center space-x-3">
-              <CheckIcon className="h-1.5 w-2.5 shrink-0 text-primary-1" />
-              <span>Country-specific rates</span>
-            </li>
-          </ul>
-          <Button
-            className="w-full"
-            to="https://calendly.com/novuhq/novu-meeting"
-            target="_blank"
-            theme="gray-outline"
-            size="sm"
-          >
-            Contact sales
-          </Button>
-        </div>
-      )}
+                </div>
+              </li>
+            );
+          }
+        )}
+        <p className="col-span-full mt-2 text-center text-sm leading-snug text-gray-8">
+          *During Open Beta, all tariffs except Enterprise are free to use.
+        </p>
+      </ul>
     </>
   );
 };
