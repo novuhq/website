a3d05e2f3b79396a3bd81151cb84295c939fe057
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-02-07 14:22:42 +0300

    feat: add og image

--- src/pages/polishing.jsx
+++ src/pages/polishing.jsx
@@ -63,6 +63,7 @@ export const Head = () => {
     title: 'Polishing Season - Novu',
     description:
       'Polishing season is about turning that “someday” into “today”. It’s about dedicating time to quality work. To replace flaws and friction with polish and delight.',
+    ogImage: `/images/social-preview-polishing.jpg`,
   };
   return <SEO {...pageMetadata} />;
 };
