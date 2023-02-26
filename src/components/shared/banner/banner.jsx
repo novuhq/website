439e14bd07c91c788558639b8a9e6d73e95ed544
Nevo David <nevo@notu.co>
2023-02-26 14:20:17 +0700

    feat: polishing season is over

--- src/components/shared/banner/banner.jsx
+++ src/components/shared/banner/banner.jsx
@@ -8,13 +8,13 @@ import illustrationRight from './images/illustration-right.svg';
 
 const Banner = () => (
   <Link
-    className="group relative z-20 block overflow-hidden bg-gray-3 py-2.5 md:py-4 md:px-7 sm:py-3.5 sm:px-4"
-    to="/polishing/"
+    className="group relative z-20 block overflow-hidden bg-gray-3 py-2.5"
+    to="/blog/seed-funding"
   >
-    <div className="relative flex items-center justify-center">
+    <div className="container relative flex items-center justify-center">
       <p className="text-highlighting-secondary-2-gradient text-center text-base sm:text-sm">
-        We are polishing, One month dedicated for{' '}
-        <span className="text-xl font-bold sm:text-base">bug fixing</span> and performance
+        Novu is <span className="text-xl font-bold sm:text-base">open-source</span>, help us by
+        starring our GitHub repo 🚀
         <ArrowIcon
           className="ml-2.5 inline-block h-2.5 w-5 transition-colors duration-200 group-hover:text-primary-1"
           aria-hidden
@@ -22,14 +22,14 @@ const Banner = () => (
       </p>
 
       <img
-        className="absolute left-3 xl:-left-12 lg:-left-40 md:-left-80 sm:-left-80"
+        className="absolute left-24 xl:left-0 lg:-left-30 md:-left-60 sm:-left-80"
         src={illustrationLeft}
         alt=""
         loading="eager"
         aria-hidden
       />
       <img
-        className="absolute right-3 xl:-right-12 lg:-right-40 md:-right-80 sm:-right-80"
+        className="absolute right-24 xl:right-0 lg:-right-30 md:-right-60 sm:-right-80"
         src={illustrationRight}
         alt=""
         loading="eager"
