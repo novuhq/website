8dd11365fbd5c1dd81c6139bf345cd14ab62704f
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-03-30 11:57:00 +0300

    chore: moving Sanity to a separate repository

--- studio/postcss.config.js
+++
@@ -1,2 +0,0 @@
-// https://github.com/sanity-io/sanity/issues/3884#issuecomment-1353828118
-module.exports = {};
--- studio/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import { defineCliConfig } from 'sanity/cli';
-
-export default defineCliConfig({
-  api: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID,
-    dataset: process.env.GATSBY_SANITY_DATASET,
-  },
-});
