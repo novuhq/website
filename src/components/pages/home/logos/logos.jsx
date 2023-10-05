3b76c9e7472db057982b4860527e0b2540955379
americano98 <mr.nik.bazarov@gmail.com>
2023-10-05 17:05:38 +0400

    chore: set bigger height for main logos

--- src/components/pages/home/logos/logos.jsx
+++ src/components/pages/home/logos/logos.jsx
@@ -103,14 +103,14 @@ const Logos = () => (
       <h2 className="text-md text-center font-book uppercase leading-none sm:text-sm sm:leading-snug">
         {TITLE}
       </h2>
-      <ul className="mx-auto mt-12 flex w-full max-w-[1200px] flex-wrap justify-center gap-y-[42px] lg:max-w-[1000px] lg:gap-y-9 md:flex-wrap md:gap-x-12 md:gap-y-7 md:space-x-0 sm:mt-6 sm:gap-x-5 sm:gap-y-5">
+      <ul className="mx-auto mt-12 flex w-full max-w-[1200px] flex-wrap items-center justify-center gap-y-[42px] lg:max-w-[1000px] lg:gap-y-9 md:flex-wrap md:gap-x-12 md:gap-y-7 md:space-x-0 sm:mt-6 sm:gap-x-5 sm:gap-y-5">
         {LOGOS.map(({ src, alt, width, height }, index) => (
           <li
             className={index < 5 ? 'mx-[39px] lg:mx-7 md:mx-0' : 'mx-[29px] lg:mx-3.5 md:mx-0'}
             key={index}
           >
             <img
-              className={clsx(index < 5 ? 'h-12 lg:h-10 md:h-8 sm:h-6' : 'h-6', 'w-auto')}
+              className={clsx(index < 5 ? 'h-12 lg:h-10 md:h-8 sm:h-7' : 'h-6', 'w-auto')}
               src={src}
               loading="eager"
               alt={alt}
