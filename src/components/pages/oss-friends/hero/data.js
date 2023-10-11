884b98eeaad1e11be2a53260a437e617858f1bfa
François-Guillaume Ribreau <git@fgribreau.com>
2023-10-11 22:53:10 +0200

    fix: add Hook0 icon

--- src/components/pages/oss-friends/hero/data.js
+++ src/components/pages/oss-friends/hero/data.js
@@ -9,6 +9,7 @@ import hankoIcon from './images/hanko.svg';
 import htmxIcon from './images/htmx.svg';
 import infisicalIcon from './images/infisical.svg';
 import openbbIcon from './images/openbb.svg';
+import hook0Icon from './images/hook0.svg';
 import sniffnetIcon from './images/sniffnet.svg';
 import typebotIcon from './images/typebot.svg';
 import webinyIcon from './images/webiny.svg';
@@ -75,7 +76,8 @@ const friendsItems = [
     name: "Hook0",
     description:
       "Open-Source Webhooks-as-a-service (WaaS) that makes it easy for developers to send webhooks.",
-    href: "https://www.hook0.com/",
+    logo: hook0Icon,
+    linkUrl: "https://www.hook0.com/",
   },
   {
     name: 'HTMX',
