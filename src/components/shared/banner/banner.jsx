ea68cf7824cf96642549aa02d10c369d98a656ee
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2024-01-16 16:00:43 +0530

    refactor: change messaging for golden kitty! 🐈

--- src/components/shared/banner/banner.jsx
+++ src/components/shared/banner/banner.jsx
@@ -10,12 +10,17 @@ const Banner = () => (
   <Link
     className="top-banner group relative z-30 block overflow-hidden bg-gray-3 py-2.5"
     target="_blank"
-    to="https://github.com/novuhq/novu"
+    to="https://www.producthunt.com/golden-kitty-awards/developer-tools"
   >
     <div className="container relative flex items-center justify-center">
       <p className="text-highlighting-secondary-2-gradient text-center text-base sm:text-sm">
-        Novu is <span className="text-xl font-bold sm:text-base">open-source</span>, help us by
-        starring our GitHub repo 🚀
+        {/* Novu is <span className="text-xl font-bold sm:text-base">open-source</span>, help us by
+        starring our GitHub repo 🚀 */}
+        {/* -----------------callout for golden kitty awards */}
+        Novu has been nominated for the{' '}
+        <span className="text-xl font-bold sm:text-base">Golden Kitty Awards!</span> Help us get in
+        the hall of fame
+        {/* ------------------------------------------------ */}
         <ArrowIcon
           className="ml-2.5 inline-block h-2.5 w-5 transition-colors duration-200 group-hover:text-primary-1"
           aria-hidden
