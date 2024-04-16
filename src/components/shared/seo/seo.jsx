38b8dd36f8d8db12b37910b893accfa6374dfe3c
unicodeveloper <prosperotemuyiwa@gmail.com>
2024-04-16 08:55:52 +0100

    feat: add linkedin analytics

--- src/components/shared/seo/seo.jsx
+++ src/components/shared/seo/seo.jsx
@@ -61,6 +61,36 @@ const SEO = ({
         <Script id="hs-script-loader" src="//js.hs-scripts.com/44416662.js" />
       )}
 
+      {/** LinkedIn analytics */}
+      {process.env.NODE_ENV === 'production' && (
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+            _linkedin_partner_id = "5915650";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `,
+          }}
+        />
+      )}
+
+      {process.env.NODE_ENV === 'production' && (
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);})(window.lintrk);
+          `,
+          }}
+        />
+      )}
+
       {process.env.NODE_ENV === 'production' && (
         <Script
           type="text/javascript"
