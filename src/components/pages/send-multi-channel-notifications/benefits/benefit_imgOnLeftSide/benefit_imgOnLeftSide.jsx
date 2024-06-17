c488850f2913b444da4e883cfd2cda6f605e0ab6
StarL0rdDude <63902456+StarL0rdDude@users.noreply.github.com>
2024-06-17 22:48:58 +0300

    init

---
+++ src/components/pages/send-multi-channel-notifications/benefits/benefit_imgOnLeftSide/benefit_imgOnLeftSide.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useInView } from 'react-intersection-observer';
+
+import Button from 'components/shared/button/button';
+import Heading from 'components/shared/heading/heading';
+import LottieAnimation from 'components/shared/lottie-animation';
+import LINKS from 'constants/links';
+
+import animationData from './data/notification-center-lottie-data.json';
+
+const Benefit_imgOnLeftSide = ({ title, description, buttonText }) => {
+  const [animationWrapperRef, isAnimationWrapperInView] = useInView({
+    threshold: 0.6,
+  });
+
+  const lottieData = {
+    lottieOptions: {
+      animationData,
+      loop: true,
+    },
+    isInView: isAnimationWrapperInView,
+  };
+
+  return (
+    <section className="notification-center safe-paddings bg-gray-2 pb-10 pt-20 lg:pb-24 lg:pt-32 md:pb-18 md:pt-28 sm:pb-12 sm:pt-18">
+      <div className="container grid-gap-x grid grid-cols-12 items-center md:flex md:flex-col">
+        <div
+          className="relative col-start-1 col-end-7 md:order-2 md:mt-11 md:w-full sm:mt-8"
+          ref={animationWrapperRef}
+        >
+          <LottieAnimation {...lottieData} />
+        </div>
+
+        <div className="col-start-8 col-end-13 xl:col-start-8 md:order-1 md:text-center">
+          <Heading
+            size="xl"
+            tag="h2"
+            className="leading-tight lg:text-4xl sm:text-3xl"
+            theme="white"
+          >
+            {title}
+          </Heading>
+          <p className="mt-5 text-lg font-book leading-snug text-gray-9 lg:mt-3 lg:text-base">
+            {description}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Benefit_imgOnLeftSide;
---
+++ src/components/pages/send-multi-channel-notifications/hero/hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Heading from 'components/shared/heading';
+
+
+const Hero = () => (
+  <section className="safe-paddings relative overflow-hidden pb-10 pt-36 lg:pb-16 lg:pt-32 md:pt-[104px] sm:pb-10 sm:pt-[95px]">
+    <div className="container-lg relative z-10">
+      <Heading
+        className="mx-auto text-center text-8xl font-bold leading-denser lg:text-6xl md:text-4xl sm:text-[32px]"
+        size="3xl"
+        tag="h1"
+        theme="white"
+        asHTML
+      >
+        Send Multi-Channel Notifications
+      </Heading>
+      <p className="mx-auto mt-4 max-w-[800px] text-center text-lg leading-tight opacity-70 lg:mt-5 lg:max-w-[676px] md:mt-4 md:max-w-[590px] md:text-base sm:mt-3">
+        Reach Your Audience Where They Are: Multi-Channel Notification Solutions
+      </p>
+    </div>
+  </section>
+);
+
+export default Hero;
