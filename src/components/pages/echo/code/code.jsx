155008d4ca70b09072dc136ca3293d41ce44381d
iatopilskii <ilya@pixelpoint.io>
2024-06-17 20:33:19 +0200

    fix: fix code section adaptive

--- src/components/pages/echo/code/code.jsx
+++ src/components/pages/echo/code/code.jsx
@@ -69,7 +69,7 @@ const Code = () => {
 
   return (
     <section className="code mt-[206px] lg:mt-36 md:mt-[116px] sm:mt-20">
-      <div className="container-lg px-8 min-h-[836px] relative lg:min-h-[692px] md:min-h-[535px] sm:min-h-[324px]">
+      <div className="container-lg px-8 min-h-[836px] relative lg:max-w-5xl lg:min-h-[692px] md:max-w-3xl md:min-h-[535px] sm:min-h-[324px]">
         <h2 className="text-[52px] leading-none tracking-snug font-medium text-transparent max-w-[500px] bg-clip-text bg-[linear-gradient(360deg,rgba(104,181,215,1)_-19.23%,#FFFFFF_30.54%)] ml-[42px] lg:text-5xl lg:ml-8 md:text-4xl md:max-w-md md:ml-0 sm:text-[32px]">
           Developer-first with&nbsp;product in mind
         </h2>
@@ -94,27 +94,25 @@ const Code = () => {
             </li>
           ))}
         </ul>
-        <SyntaxHighlighter
-          className="echo-code pl-[42px] mt-[70px] scrollbar-hidden text-sm font-normal lg:mt-[62px] lg:pl-[35px] lg:text-xs md:text-[9px] md:mt-[53px] md:pl-[26px] sm:text-[4px] sm:mt-10 sm:pl-3"
-          language="javascript"
-          useInlineStyles={false}
-          renderer={customRenderer}
-          showLineNumbers
-        >
-          {code}
-        </SyntaxHighlighter>
-        <StaticImage
-          className="!absolute bottom-0 right-6 -z-10 w-[1262px] lg:w-[1052px] md:w-[773px] sm:w-[352px] sm:right-4"
-          src="./images/code-background.png"
-          alt=""
-          width={1262}
-          height={766}
-          quality={100}
-        />
-        <div
-          className="absolute bottom-0 right-6 w-[1152px] h-[652px] rounded-[20px] bg-[linear-gradient(180deg,rgba(5,5,11,0)_47.56%,rgba(5,5,11,0.1)_74.21%,#05050B_97.67%)] pointer-events-none lg:w-[960px] lg:right-8 lg:h-[548px] md:w-[704px] md:h-[418px] sm:w-[320px] sm:h-[192px] sm:right-5"
-          aria-hidden
-        />
+        <div className="relative lg:mx-auto sm:max-w-80 sm:mx-auto">
+          <SyntaxHighlighter
+            className="echo-code pl-[42px] mt-[70px] scrollbar-hidden text-sm font-normal lg:mt-[62px] lg:pl-[35px] lg:text-xs md:text-[9px] md:mt-[53px] md:pl-[26px] sm:text-[4px] sm:mt-10 sm:pl-3"
+            language="javascript"
+            useInlineStyles={false}
+            renderer={customRenderer}
+            showLineNumbers
+          >
+            {code}
+          </SyntaxHighlighter>
+          <StaticImage
+            className="!absolute bottom-[-85px] -right-2.5 -z-10 w-[1262px] lg:bottom-[-60px] lg:w-[1052px] md:-right-2 md:bottom-[-37px] md:w-[773px] sm:w-[352px] sm:-right-1 sm:bottom-[-21px]"
+            src="./images/code-background.png"
+            alt=""
+            width={1262}
+            height={766}
+            quality={100}
+          />
+        </div>
         <div
           className="absolute left-[-90px] top-[90px] h-[348px] w-[490px] rounded-[50%] bg-[radial-gradient(88.94%_88.94%_at_62.86%_11.06%,#3BDCFF_27.2%,#69B7FF_80.5%,#4759FF_100%)] opacity-15 blur-3xl scale-[1.5] -z-20 pointer-events-none lg:w-[408px] lg:h-[290px] md:w-[300px] md:h-[212px] sm:w-[136px] sm:h-[96px] sm:left-[-20px] sm:top-[120px]"
           aria-hidden
