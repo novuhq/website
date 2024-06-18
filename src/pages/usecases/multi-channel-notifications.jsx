e7b530c568436eae0731237f5b8d03c1d9dfc2f4
StarL0rdDude <63902456+StarL0rdDude@users.noreply.github.com>
2024-06-18 15:19:10 +0300

    quick fix

--- src/pages/usecases/multi-channel-notifications.jsx
+++ src/pages/usecases/multi-channel-notifications.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 //Imprort page-specific components
-import Hero from 'components/pages/send-multi-channel-notifications/hero';
-import Pain_restatement from 'components/pages/send-multi-channel-notifications/painRestatement';
-import Benefits from 'components/pages/send-multi-channel-notifications/benefits';
-import Features from 'components/pages/send-multi-channel-notifications/features';
-import RelatedContent from 'components/pages/send-multi-channel-notifications/related-content/related-content';
+import Hero from 'components/pages/channel-notifications/hero';
+import Pain_restatement from 'components/pages/multi-channel-notifications/painRestatement';
+import Benefits from 'components/pages/multi-channel-notifications/benefits';
+import Features from 'components/pages/multi-channel-notifications/features';
+import RelatedContent from 'components/pages/multi-channel-notifications/related-content/related-content';
 
 //Import shared (cross-page) components
 import Layout from 'components/shared/layout';
@@ -36,8 +36,8 @@ export default MultiChannelNotificationsPage;
 
 export const Head = () => {
     const pageMetadata = {
-        slug: '/send-multi-channel-notifications/',
-        title: 'Send Multi-Channel Notifications',
+        slug: '/multi-channel-notifications/',
+        title: 'Multi-Channel Notifications',
         description: 'Reach Your Audience Where They Are: Multi-Channel Notification Solutions',
     };
     return <SEO {...pageMetadata} />;
