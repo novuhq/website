0c8d66f8a6389073bb57391a0c9fbfc35eb03d3e
Justin Nemmers <justnems@users.noreply.github.com>
2024-07-16 12:02:11 -0400

    Create a new thank you page for discovery session pages (#235)

---
+++ src/pages/thank-you-discovery-session.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Logos from 'components/pages/home/logos';
+import Hero from 'components/pages/thank-you-discovery-session/hero';
+import JoinUs from 'components/shared/join-us';
+import Layout from 'components/shared/layout';
+import SEO from 'components/shared/seo';
+import Separator from 'components/shared/separator';
+
+const ThankYouPage = () => (
+  <Layout>
+    <Hero />
+    <Logos />
+    <JoinUs />
+    <Separator backgroundColor="black" />
+  </Layout>
+);
+
+export default ThankYouPage;
+
+export const Head = () => {
+  const pageMetadata = {
+    preventIndexing: true,
+    title: 'Novu - Discovery session booked',
+    description: "We're looking forward to chatting soon.",
+  };
+
+  return <SEO {...pageMetadata} />;
+};
