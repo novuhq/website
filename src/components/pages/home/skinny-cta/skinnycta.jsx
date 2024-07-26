ab8b645214486e5131cbda5dc6207484d5245f6e
Justin Nemmers <justnems@users.noreply.github.com>
2024-07-26 07:13:24 -0400

    chore: update hero and CTAs (#242)
    Updated Hero text, moved several homepage CTAs to "Contact Us" instead
    of book a meeting.

--- src/components/pages/home/skinny-cta/skinnycta.jsx
+++ src/components/pages/home/skinny-cta/skinnycta.jsx
@@ -72,13 +72,13 @@ const SkinnyCTA = () => {
               />
             </div>
             <Button
-              className="text-sm h-14 min-w-[178px] -mt-px lg:h-12 sm:hover:bg-transparent sm:max-w-none sm:border-none sm:h-auto sm:text-[13px] sm:text-primary-1 sm:underline sm:underline-offset-[6px] sm:mt-[18px]"
+              className="text-sm h-14 min-w-[190px] -mt-px lg:h-12 sm:hover:bg-transparent sm:max-w-none sm:border-none sm:h-auto sm:text-[13px] sm:text-primary-1 sm:underline sm:underline-offset-[6px] sm:mt-[18px]"
               theme="gray-outline"
               size="lg"
               {...LINKS.getStarted}
               onClick={buttonClick('book_a_call', { type: 'homepage' })}
             >
-              Create Account
+              Create Free Account
             </Button>
           </div>
         </div>
