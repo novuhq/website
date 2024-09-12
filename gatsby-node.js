868f42fd973614cb9a8f82a01a03cbfae5a75ed6
iatopilskii <iatopilskii@gmail.com>
2024-09-12 20:23:57 +0200

    fix: try to fix rive loading errors

--- gatsby-node.js
+++ gatsby-node.js
@@ -640,19 +640,7 @@ exports.onPostBuild = require('./gatsby/on-post-build');
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     module: {
-      rules: [
-        {
-          test: (filePath) => {
-            const fileName = filePath.split('/').pop();
-
-            return fileName === 'rive.wasm';
-          },
-          type: 'asset/resource',
-          generator: {
-            filename: 'static/[name].[hash][ext]',
-          },
-        },
-      ],
+      rules: [{ test: /\.(riv)$/i, type: 'asset/resource' }],
     },
   });
 };
