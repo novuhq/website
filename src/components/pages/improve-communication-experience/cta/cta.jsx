f93039c2e417c7aac29ad9ac5f8d47b8d6a39a2f
Justin Nemmers <justin@nemmers.io>
2024-09-20 11:14:21 -0400

    chore: update meeting link
    Moves meeting link from group-based to RR

--- src/components/pages/improve-communication-experience/cta/cta.jsx
+++ src/components/pages/improve-communication-experience/cta/cta.jsx
@@ -12,7 +12,7 @@ import buttonClick from 'utils/use-landing-simple-tracking';
 
 const LINK = {
   text: 'Book Meeting',
-  url: 'https://notify.novu.co/meetings/novumeet/discovery-session?utm_campaign=website-usecase-improveComms',
+  url: 'https://notify.novu.co/meetings/novuhq/novu-discovery-session-rr?utm_campaign=website-usecase-improveComms',
   target: '_blank',
 };
 
