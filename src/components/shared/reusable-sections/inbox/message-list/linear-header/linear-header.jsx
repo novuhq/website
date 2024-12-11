1005d5af0554191269b585ddb03df5c9c334ce5d
Olga <olga.volotovich@mail.ru>
2024-12-11 09:45:07 +0100

    fix: focus and active menus visibility

--- src/components/shared/reusable-sections/inbox/message-list/linear-header/linear-header.jsx
+++ src/components/shared/reusable-sections/inbox/message-list/linear-header/linear-header.jsx
@@ -13,7 +13,7 @@ const THEMES = {
     titleColor: 'text-white',
     moreIconsColor: 'text-[#A5A4A1]',
     settingsIconColor: 'text-[#A5A4A1]',
-    mainIconsUnderlayStyles: 'bg-[#21222B]',
+    mainIconsUnderlayStyles: 'hover:bg-[#21222B]',
     menuStyles:
       'bg-[#23242E] shadow-[0px 4px 8px 0px rgba(19, 22, 29, 0.60)] border border-[#373847]',
     menuItemStyles: 'text-[#E5E6EF] hover:bg-[#343543] hover:text-white',
@@ -34,7 +34,7 @@ const THEMES = {
     titleColor: 'text-[#1B1B1B]',
     moreIconsColor: 'text-[#1B1B1B]',
     settingsIconColor: 'text-[#5D5D5E]',
-    mainIconsUnderlayStyles: 'bg-[#F1F1F1] hover:text-white',
+    mainIconsUnderlayStyles: 'hover:bg-[#F1F1F1] hover:text-white',
     menuStyles:
       'bg-white shadow-[0px_4px_10px_0px_rgba(53,53,53,0.15),_0px_1px_2px_0px_rgba(53,53,53,0.20)] border border-[#DCDCDC]',
     menuItemStyles: 'text-[#303031] hover:bg-[#F3F3F3] hover:text-[#1B1B1B]',
@@ -97,25 +97,27 @@ const LinearHeader = ({
         >
           Inbox
         </span>
-        <span className="group relative flex items-center" role="button" tabIndex="0">
-          <div
+        <div className="relative">
+          <button
             className={clsx(
-              'invisible absolute left-1/2 top-1/2 size-7 -translate-x-1/2 -translate-y-1/2 rounded-[6px] opacity-0 transition-[opacity,visibility] duration-200 group-hover:visible group-hover:opacity-100',
+              'menu-btn relative flex size-7 items-center justify-center rounded-[6px] bg-transparent transition-colors before:absolute before:left-0 before:top-0 before:size-11 before:bg-transparent',
               currentTheme.mainIconsUnderlayStyles
             )}
-          />
-          <LinearMoreIcon
-            className={clsx('z-10 size-5', currentTheme.moreIconsColor)}
-            aria-hidden
-          />
+            type="button"
+          >
+            <LinearMoreIcon
+              className={clsx('z-10 size-5', currentTheme.moreIconsColor)}
+              aria-hidden
+            />
+          </button>
           <ul
             className={clsx(
-              'invisible absolute left-0 top-[calc(100%+10px)] z-10 min-w-[280px] rounded-[10px] p-1.5 pt-[5px] font-inter opacity-0 transition-[opacity,visibility] duration-300 group-focus-within:visible group-focus-within:opacity-100 group-hover:visible group-hover:opacity-100',
+              'menu invisible absolute left-0 top-[calc(100%+10px)] z-10 min-w-[280px] rounded-[10px] p-1.5 pt-[5px] font-inter opacity-0 transition-[opacity,visibility] duration-300',
               currentTheme.menuStyles
             )}
           >
             {actions.map(({ label, Icon, action }, index) => (
-              <li key={index}>
+              <li className="menu-item" key={index}>
                 {index === 1 && (
                   <div
                     className={clsx(
@@ -139,137 +141,135 @@ const LinearHeader = ({
               </li>
             ))}
           </ul>
-        </span>
+        </div>
       </div>
-      <span className="group relative flex items-center" role="button" tabIndex="0">
-        <div
-          className={clsx(
-            'invisible absolute left-1/2 top-1/2 size-7 -translate-x-1/2 -translate-y-1/2 rounded-[6px] opacity-0 transition-[opacity,visibility] duration-200 group-hover:visible group-hover:opacity-100',
-            currentTheme.mainIconsUnderlayStyles
-          )}
-        />
+      <button
+        className={clsx(
+          'menu-btn relative size-7 items-center justify-center rounded-[6px] bg-transparent transition-colors before:absolute before:left-0 before:top-0 before:size-11 before:bg-transparent'
+        )}
+        type="button"
+      >
         <LinearSettingsIcon
           className={clsx('z-10 size-5', currentTheme.settingsIconColor)}
           aria-hidden
         />
-
-        <div
-          className={clsx(
-            'invisible absolute right-0 top-[calc(100%+12px)] min-w-[306px] rounded-[10px] px-[19px] pb-[17px] pt-3 font-inter opacity-0 transition-[opacity,visibility] duration-300 group-focus-within:visible group-focus-within:opacity-100 group-hover:visible group-hover:opacity-100',
-            currentTheme.orderingMenuStyles
-          )}
-          onMouseLeave={() => setIsExpanded(false)}
-        >
-          <div className="relative flex items-center justify-between gap-x-2">
-            <div className="flex items-center gap-x-3">
-              <OrderingIcon className="size-4 shrink-0" aria-hidden />
-              <span>Ordering</span>
+      </button>
+      <div
+        className={clsx(
+          'menu invisible absolute right-0 top-[calc(100%+12px)] min-w-[306px] rounded-[10px] px-[19px] pb-[17px] pt-3 font-inter opacity-0 transition-[opacity,visibility] duration-300',
+          currentTheme.orderingMenuStyles
+        )}
+        onMouseLeave={() => setIsExpanded(false)}
+      >
+        <div className="relative flex items-center justify-between gap-x-2">
+          <div className="flex items-center gap-x-3">
+            <OrderingIcon className="size-4 shrink-0" aria-hidden />
+            <span>Ordering</span>
+          </div>
+          <button
+            className={clsx(
+              'flex items-center gap-x-2.5 rounded-[6px] px-2.5 py-1.5 font-inter text-[16px] leading-none',
+              currentTheme.orderingDropdownBtnStyles
+            )}
+            type="button"
+            onClick={handleSortingClick}
+          >
+            <span className="">{orderingPosition}</span>
+            <DownArrowIcon className="size-2" aria-hidden />
+          </button>
+          {isExpanded && (
+            <div
+              className={clsx(
+                'absolute right-0 top-0 z-10 flex flex-col gap-y-2 rounded-[6px] border-[0.5px] p-2.5',
+                currentTheme.orderingDropdownStyles
+              )}
+            >
+              <button
+                className="relative pl-6"
+                type="button"
+                onClick={() => handleOrderingClick(ordering[0].label)}
+              >
+                {orderingPosition === ordering[0].label && (
+                  <CheckSelectedIcon
+                    className={clsx(
+                      'absolute left-0 top-1/2 size-3.5 -translate-y-1/2',
+                      currentTheme.orderingDropdownIconStyles
+                    )}
+                    aria-hidden
+                  />
+                )}
+                {ordering[0].label}
+              </button>
+              <button
+                className="relative pl-6"
+                type="button"
+                onClick={() => handleOrderingClick(ordering[1].label)}
+              >
+                {orderingPosition === ordering[1].label && (
+                  <CheckSelectedIcon
+                    className={clsx(
+                      'absolute left-0 top-1/2 size-3.5 -translate-y-1/2',
+                      currentTheme.orderingDropdownIconStyles
+                    )}
+                    aria-hidden
+                  />
+                )}
+                {ordering[1].label}
+              </button>
             </div>
+          )}
+        </div>
+        <span
+          className={clsx(
+            'absolute inset-x-0 top-[54px] block h-px',
+            currentTheme.menuSeparatorStyles
+          )}
+          aria-hidden
+        />
+        <div className="mt-[31px] flex flex-col justify-between gap-y-6">
+          <div className="flex items-center justify-between text-[16px] leading-none">
+            <p>Show unread first</p>
             <button
               className={clsx(
-                'flex items-center gap-x-2.5 rounded-[6px] px-2.5 py-1.5 font-inter text-[16px] leading-none',
-                currentTheme.orderingDropdownBtnStyles
+                'relative h-5 w-8 rounded-full px-0.5 py-[3px] transition-colors duration-300 ease-in-out',
+                showUnreadFirst ? currentTheme.toggleOnBgColor : currentTheme.toggleOffBgColor
               )}
               type="button"
-              onClick={handleSortingClick}
+              aria-label={`Switcher to show unread messages first. Now ${
+                showUnreadFirst ? 'on' : 'off'
+              }`}
+              onClick={toggleShowUnreadFirst}
             >
-              <span className="">{orderingPosition}</span>
-              <DownArrowIcon className="size-2" aria-hidden />
+              <span
+                className={clsx(
+                  'absolute top-1/2 size-3.5 -translate-y-1/2 rounded-full bg-white transition-transform duration-300 ease-in-out',
+                  showUnreadFirst ? 'translate-x-0' : '-translate-x-full'
+                )}
+              />
             </button>
-            {isExpanded && (
-              <div
-                className={clsx(
-                  'absolute right-0 top-0 z-10 flex flex-col gap-y-2 rounded-[6px] border-[0.5px] p-2.5',
-                  currentTheme.orderingDropdownStyles
-                )}
-              >
-                <button
-                  className="relative pl-6"
-                  type="button"
-                  onClick={() => handleOrderingClick(ordering[0].label)}
-                >
-                  {orderingPosition === ordering[0].label && (
-                    <CheckSelectedIcon
-                      className={clsx(
-                        'absolute left-0 top-1/2 size-3.5 -translate-y-1/2',
-                        currentTheme.orderingDropdownIconStyles
-                      )}
-                      aria-hidden
-                    />
-                  )}
-                  {ordering[0].label}
-                </button>
-                <button
-                  className="relative pl-6"
-                  type="button"
-                  onClick={() => handleOrderingClick(ordering[1].label)}
-                >
-                  {orderingPosition === ordering[1].label && (
-                    <CheckSelectedIcon
-                      className={clsx(
-                        'absolute left-0 top-1/2 size-3.5 -translate-y-1/2',
-                        currentTheme.orderingDropdownIconStyles
-                      )}
-                      aria-hidden
-                    />
-                  )}
-                  {ordering[1].label}
-                </button>
-              </div>
-            )}
           </div>
-          <span
-            className={clsx(
-              'absolute inset-x-0 top-[54px] block h-px',
-              currentTheme.menuSeparatorStyles
-            )}
-            aria-hidden
-          />
-          <div className="mt-[31px] flex flex-col justify-between gap-y-6">
-            <div className="flex items-center justify-between text-[16px] leading-none">
-              <p>Show unread first</p>
-              <button
-                className={clsx(
-                  'relative h-5 w-8 rounded-full px-0.5 py-[3px] transition-colors duration-300 ease-in-out',
-                  showUnreadFirst ? currentTheme.toggleOnBgColor : currentTheme.toggleOffBgColor
-                )}
-                type="button"
-                aria-label={`Switcher to show unread messages first. Now ${
-                  showUnreadFirst ? 'on' : 'off'
-                }`}
-                onClick={toggleShowUnreadFirst}
-              >
-                <span
-                  className={clsx(
-                    'absolute top-1/2 size-3.5 -translate-y-1/2 rounded-full bg-white transition-transform duration-300 ease-in-out',
-                    showUnreadFirst ? 'translate-x-0' : '-translate-x-full'
-                  )}
-                />
-              </button>
-            </div>
 
-            <div className="flex items-center justify-between text-[16px] leading-none">
-              <p>Show read</p>
-              <button
+          <div className="flex items-center justify-between text-[16px] leading-none">
+            <p>Show read</p>
+            <button
+              className={clsx(
+                'relative h-[20px] w-[32px] rounded-full px-[2px] py-[3px] transition-colors duration-300 ease-in-out',
+                showRead ? currentTheme.toggleOnBgColor : currentTheme.toggleOffBgColor
+              )}
+              type="button"
+              aria-label={`Switch to show read messages. Now ${showRead ? 'on' : 'off'}`}
+              onClick={toggleShowRead}
+            >
+              <span
                 className={clsx(
-                  'relative h-[20px] w-[32px] rounded-full px-[2px] py-[3px] transition-colors duration-300 ease-in-out',
-                  showRead ? currentTheme.toggleOnBgColor : currentTheme.toggleOffBgColor
+                  'absolute top-1/2 size-3.5 -translate-y-1/2 rounded-full bg-white transition-transform duration-300 ease-in-out',
+                  showRead ? 'translate-x-0' : '-translate-x-full'
                 )}
-                type="button"
-                aria-label={`Switch to show read messages. Now ${showRead ? 'on' : 'off'}`}
-                onClick={toggleShowRead}
-              >
-                <span
-                  className={clsx(
-                    'absolute top-1/2 size-3.5 -translate-y-1/2 rounded-full bg-white transition-transform duration-300 ease-in-out',
-                    showRead ? 'translate-x-0' : '-translate-x-full'
-                  )}
-                />
-              </button>
-            </div>
+              />
+            </button>
           </div>
         </div>
-      </span>
+      </div>
       <div
         className={clsx(
           currentTheme.headerSeparator,
