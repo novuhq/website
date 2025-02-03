de4b9ab917c2d6f4fddba00ebb3b1e548bf0b029
Anna Artemova <annartmva@gmail.com>
2025-02-03 18:35:27 +0400

    fix: mobile menu, ultra-wide screens, external links

--- src/components/shared/link-banner/link-banner.jsx
+++ src/components/shared/link-banner/link-banner.jsx
@@ -5,24 +5,31 @@ import bannerData from 'data/link-banner';
 
 import bgImage from './images/banner-bg.jpg';
 
+const outsideSettings = {
+  target: '_blank',
+  rel: 'noopener noreferrer',
+};
+
 const LinkBanner = () => {
   const { text, url, isActive } = bannerData;
+  const isExternal = !url.startsWith('/');
 
   if (!isActive) return null;
 
   return (
-    <section className="sticky top-0 z-50 overflow-hidden bg-[#0B0C0F]">
+    <section className="link-banner sticky top-0 z-50 overflow-hidden bg-[#0B0C0F] after:absolute after:inset-x-0 after:bottom-0 after:h-px after:w-full after:bg-white after:mix-blend-overlay">
       <Link
-        className="relative z-10 h-9 w-full justify-center"
+        className="relative z-20 h-9 w-full justify-center"
         to={url}
         size="sm"
         theme="white"
+        {...(isExternal && outsideSettings)}
         withArrow
       >
         {text}
       </Link>
       <img
-        className="absolute inset-y-0 left-1/2 h-auto min-w-[1920px] -translate-x-1/2 xs:-translate-x-[45%] sm-xs:-translate-x-[40%]"
+        className="absolute inset-y-0 left-1/2 z-10 h-9 min-w-[1920px] -translate-x-1/2 object-bottom xs:-translate-x-[45%] sm-xs:-translate-x-[40%]"
         src={bgImage}
         alt=""
         loading="eager"
