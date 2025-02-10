07b4c488dee7ad5fef0da48f1dc90e7c1a3bab95
Olga <olga.volotovich@mail.ru>
2025-02-10 07:22:15 +0100

    refactor: delete GetInvolved reusable comp

--- src/pages/thank-you-meeting.jsx
+++ src/pages/thank-you-meeting.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import Hero from 'components/pages/thank-you-meeting/hero';
 import Layout from 'components/shared/layout';
-import GetInvolved from 'components/shared/reusable-sections/get-involved';
+import SectionWithBigIcons from 'components/shared/reusable-sections/section-with-big-icons';
 import SectionWithLogos from 'components/shared/reusable-sections/section-with-logos';
 import SEO from 'components/shared/seo';
 import Separator from 'components/shared/separator';
-import GET_INVOLVED from 'data/pages/thank-you-discovery-session/get-involved';
+import SECTION_WITH_BIG_ICONS from 'data/pages/thank-you-discovery-session/section-with-big-icons';
 import SECTION_WITH_LOGOS from 'data/pages/thank-you-discovery-session/section-with-logos';
 
 const ThankYouPage = () => (
@@ -17,10 +17,11 @@ const ThankYouPage = () => (
       description={SECTION_WITH_LOGOS.description}
       logos={SECTION_WITH_LOGOS.logos}
     />
-    <GetInvolved
-      title={GET_INVOLVED.title}
-      items={GET_INVOLVED.items}
-      bottomMargin="mb-40 lg:mb-[120px] md:mb-[100px] sm:mb-20"
+    <SectionWithBigIcons
+      className="mb-40 lg:mb-30 md:mb-[100px] sm:mb-20"
+      title={SECTION_WITH_BIG_ICONS.title}
+      items={SECTION_WITH_BIG_ICONS.items}
+      isCentered
     />
     <Separator backgroundColor="black" />
   </Layout>
