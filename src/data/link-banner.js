77b3a74cd8cffb6c0eb348272f49112a466fb5b0
Anna Artemova <annartmva@gmail.com>
2025-02-11 18:34:03 +0400

    feat: update top banner

--- src/data/link-banner.js
+++ src/data/link-banner.js
@@ -1,7 +1,8 @@
 const bannerData = {
-  text: 'Winter Season Launch Week is happening this week, Read More',
+  text: 'Winter Season Launch Week is happening this week',
   url: 'https://go.novu.co/gFfjEj4?utm_source=web-top-banner',
   isActive: true,
+  isReadMoreText: true,
 };
 
 export default bannerData;
