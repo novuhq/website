f452f561558ab17292975c07192b1b5eaaa3822e
Dima Grossman <dima@grossman.io>
2025-03-19 15:43:40 +0200

    Revert "feat: add ph banner"
    This reverts commit 442e663f2e5549b7aba5c261067b73ef45c826b2.

--- src/data/link-banner.js
+++ src/data/link-banner.js
@@ -1,7 +1,7 @@
 const bannerData = {
-  text: 'Novu is Live on ProductHunt, click to support us today!',
-  url: 'https://go.novu.co/hunt?utm_source=web-top-banner',
-  isActive: true,
+  text: 'Winter Season Launch Week is happening this week',
+  url: 'https://go.novu.co/gFfjEj4?utm_source=web-top-banner',
+  isActive: false,
   isReadMoreText: true,
 };
 
