24ee35f9023ac9febd8daf54c8163f0d615c9cd3
Saimon Kataev <saimon.kat23@gmail.com>
2025-03-25 17:29:41 +0100

    feat: add fallback data for github

--- src/constants/github.js
+++ src/constants/github.js
@@ -1,10 +1,12 @@
-import LINKS from 'constants/links.js';
-
+// Fallback data, updated on 25/03/2025
 const GITHUB = {
-  userName: 'novuhq',
-  repoName: 'novu',
-  apiUrl: 'https://api.github.com/repos',
-  repoUrl: LINKS.github.to,
+  count: 36597,
+  commits: 19511,
+  closedIssues: 7544,
+  contributors: 2470,
+  forks: 3986,
+  pullRequests: 6254,
+  openIssues: 268,
 };
 
 export default GITHUB;
