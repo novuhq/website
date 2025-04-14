a9848a563ba24aca79ca21f1e5dfe7361c4b57ee
Anna Artemova <annartmva@gmail.com>
2025-04-14 09:46:37 +0400

    fix: scroll issue for route change

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,10 +6,23 @@ export const onRouteUpdate = () => {
   }
 };
 
-export const shouldUpdateScroll = ({ routerProps: { location } }) => {
+export const shouldUpdateScroll = ({ routerProps: { location }, getSavedScrollPosition }) => {
   if (location.state && location.state.preventScroll === true) {
     return false;
   }
 
-  return true;
+  window.history.scrollRestoration = 'manual';
+  const currentPosition = getSavedScrollPosition(location, location.key);
+
+  if (!currentPosition) {
+    window.scrollTo(0, 0);
+  } else {
+    window.setTimeout(() => {
+      window.requestAnimationFrame(() => {
+        window.scrollTo(...currentPosition);
+      });
+    }, 0);
+  }
+
+  return false;
 };
