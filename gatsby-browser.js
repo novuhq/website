86ff51405cb307959d5c67b00834e5fb6de2e1e9
Anna Artemova <annartmva@gmail.com>
2025-04-15 13:03:45 +0400

    fix: scroll-to-top issue on route change (#341)
    **Describe what changes this pull request brings**
    
    Fix scroll-to-top issue on route change in Gatsby
    
    Addresses an issue where `scroll-behavior: smooth` causes the
    scroll-to-top action to be interrupted during rehydration, due to layout
    shifts from dynamically loaded DOM elements
    
    **Steps to check:**
    
    1. Open preview: [Home
    page](https://deploy-preview-341--novu-website.netlify.app/), [URL with
    hash](https://deploy-preview-341--novu-website.netlify.app/landing/get-started/#video)
    2. Make sure that everything looks and works as expected
    
    ---------
    
    Co-authored-by: Saimon Kataev <saimon.kat23@gmail.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,10 +6,32 @@ export const onRouteUpdate = () => {
   }
 };
 
-export const shouldUpdateScroll = ({ routerProps: { location } }) => {
+export const shouldUpdateScroll = ({ routerProps: { location }, getSavedScrollPosition }) => {
   if (location.state && location.state.preventScroll === true) {
     return false;
   }
 
-  return true;
+  if (location.hash) {
+    window.history.scrollRestoration = 'auto';
+    return true;
+  }
+
+  // Fix for Gatsby 5 issue with scroll-behavior - https://github.com/gatsbyjs/gatsby/issues/38201
+
+  window.history.scrollRestoration = 'manual';
+
+  const currentPosition = getSavedScrollPosition(location, location.key);
+  const top = currentPosition ? currentPosition[1] : 0;
+
+  window.setTimeout(() => {
+    window.requestAnimationFrame(() => {
+      try {
+        window.scrollTo({ top, behavior: 'instant' });
+      } catch (e) {
+        window.scrollTo(top);
+      }
+    });
+  }, 0);
+
+  return false;
 };
