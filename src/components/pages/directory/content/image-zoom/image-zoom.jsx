38681287d44d1c3d201d9fb3e3885415043518ac
Olga <olga.volotovich@mail.ru>
2025-06-04 14:43:29 +0200

    feat: responsive margin adjustments for zoomed image

--- src/components/pages/directory/content/image-zoom/image-zoom.jsx
+++ src/components/pages/directory/content/image-zoom/image-zoom.jsx
@@ -1,14 +1,39 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { getImage } from 'gatsby-plugin-image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
 const ImageZoom = ({ image, children }) => {
   const imageUrl = getImage(image);
+  const [zoomMargin, setZoomMargin] = useState(16);
+
+  useEffect(() => {
+    const updateMargin = () => {
+      const width = window.innerWidth;
+
+      if (width <= 1024) {
+        setZoomMargin(16);
+      } else if (width <= 1920) {
+        setZoomMargin(80);
+      } else {
+        setZoomMargin(260);
+      }
+    };
+
+    updateMargin();
+    window.addEventListener('resize', updateMargin);
+
+    return () => window.removeEventListener('resize', updateMargin);
+  }, []);
 
   return (
-    <Zoom classDialog="zoom-modal" zoomImg={{ src: imageUrl }} zoomMargin={16} wrapElement="span">
+    <Zoom
+      classDialog="zoom-modal"
+      zoomImg={{ src: imageUrl }}
+      zoomMargin={zoomMargin}
+      wrapElement="span"
+    >
       {children}
     </Zoom>
   );
